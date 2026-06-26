cf9da9f9b74e89d69789c6dc5c2b6f24e0121a5f
Jyoti Pal <amjyo9611@gmail.com>
2026-06-26 23:50:55 +0530

    Mail for Order system

--- resources/views/emails/admin/low_stock.blade.php
+++ resources/views/emails/admin/low_stock.blade.php
@@ -1,24 +1,29 @@
 <x-mail::message>
+
 @if($product->stock == 0)
-# Out of Stock Alert
+# Out of Stock Alert 🚨
+
+Hi **Admin**,
 
 The following product is completely out of stock and unavailable for purchase!
 @else
-# Low Stock Alert
+# Low Stock Alert ⚠️
+
+Hi **Admin**,
 
 The following product is running low on stock and needs to be replenished.
 @endif
 
-**Product Details:**
-- Name: {{ $product->name }}
-- Current Stock: **{{ $product->stock }}**
+### Product Details
+**Name:** {{ $product->name }}<br>
+**Current Stock:** **{{ $product->stock }}**
 
 Please update the inventory in the admin panel as soon as possible.
 
-<x-mail::button :url="url('/admin/products/' . $product->id . '/edit')">
+<x-mail::button :url="url('/admin/products/' . $product->id . '/edit')" color="primary">
 Update Stock
 </x-mail::button>
 
-Thanks,<br>
-{{ config('app.name') }}
+Warm regards,<br>
+**The ALPHA DIGITAL SREES System**
 </x-mail::message>
--- resources/views/emails/order_shipped.blade.php
+++ resources/views/emails/order_shipped.blade.php
@@ -1,26 +1,27 @@
 <x-mail::message>
+
 # Great news! Your order is on the way 🚚
 
-Hi {{ $order->customer->name }},
+Hi **{{ $order->customer->name ?? 'Customer' }}**,
 
-Your order **#{{ $order->order_number }}** has just shipped! 
+Your order **#{{ $order->order_number }}** has just shipped and is on its way to you! 
 
 @if($order->expected_delivery_date)
-**Estimated Delivery Date:** {{ \Carbon\Carbon::parse($order->expected_delivery_date)->format('M d, Y') }}
+**Estimated Delivery Date:** {{ \Carbon\Carbon::parse($order->expected_delivery_date)->format('M d, Y') }}<br>
 @endif
-
 @if($order->tracking_number && $order->courier_partner)
-**Shipping Details:**
-- Courier: {{ $order->courier_partner }}
-- Tracking Number: {{ $order->tracking_number }}
+**Courier:** {{ $order->courier_partner }}<br>
+**Tracking Number:** {{ $order->tracking_number }}
 @elseif($order->tracking_number)
 **Tracking Number:** {{ $order->tracking_number }}
 @endif
 
-<x-mail::button :url="route('profile.orders.track', $order->id)">
+You can track the progress of your shipment by clicking the button below.
+
+<x-mail::button :url="route('profile.orders.track', $order->id)" color="primary">
 Track Your Order
 </x-mail::button>
 
-Thanks,<br>
-{{ config('app.name') }}
+Warm regards,<br>
+**The ALPHA DIGITAL SREES Team**
 </x-mail::message>
